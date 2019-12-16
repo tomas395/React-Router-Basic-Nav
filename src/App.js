95bb1bf1fdbd5a2658959161c5c29b072879a57c
Tomas Ortiz <TomasOrtiz@protonmail.com>
2019-12-16 13:04:02 -0800

    MVP?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import "./App.css";
+import { Home, About, Contact, Navigation } from "./components";
+import { BrowserRouter as Route } from "react-router-dom";
 
-const App = () => (
-  <div>
-    <Navigation />
-  </div>
-);
+const App = () => {
+  return (
+    <div className="App">
+      <Navigation />
+      <Route exact path="/"></Route>
+      <Route path="/about"></Route>
+      <Route path="/contact"></Route>
+    </div>
+  );
+};
 
 export default App;
