ff9236fcd01e7fab8cc9440be387bc9c73fc4230
Tomas Ortiz <TomasOrtiz@protonmail.com>
2019-12-16 13:28:17 -0800

    fixed 3 warnings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Home, About, Contact, Navigation } from "./components";
+import { Navigation } from "./components";
 import { BrowserRouter as Route } from "react-router-dom";
 
 const App = () => {
